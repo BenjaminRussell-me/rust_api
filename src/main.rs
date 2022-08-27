638c613d9ba479d50568573131f8875a8387770a
Benjamin <contact@benjaminrussell.me>
2022-08-27 10:22:54 -0500

    I gave up and did postgres

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,32 @@
-use actix_web::{
-    HttpServer,
-    HttpResponse,
-    get,
-    App,
-    web::Path,
-    Responder,
-};
-use rhai::Engine;
-use listenfd::ListenFd;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
+
+use actix_web::{App, HttpServer};
 use dotenv::dotenv;
+use listenfd::ListenFd;
 use std::env;
 
+mod api_error;
+mod db;
+mod schema;
 mod user;
 
-#[get("/add/{num1}/{num2}")]
-async fn add(path: Path<(i64, i64)>) -> impl Responder {
-    // get the numbers from the url path
-    let (num1, num2) = path.into_inner();
-
-    // create an instance of the rhai engine
-    let mut engine = Engine::new();
-
-    // register an API that exposes the numbers to Rhai
-    engine.register_fn("num1", move || num1);
-    engine.register_fn("num2", move || num2);
-
-    // run the script
-    let result = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
-
-    // return the result
-    HttpResponse::Ok().body(result.to_string())
-}
-
-// When Rust executes the Rhai script, Rhai returns the result of the last expression
-
-#[actix_web::main]
+#[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init();
-    
-    let mut listenfd = ListenFd::from_env();
 
-    let mut server =HttpServer::new(|| {
+    db::init();
+
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| 
         App::new()
             .configure(user::init_routes)
-    });
+    );
 
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
@@ -54,5 +36,7 @@ async fn main() -> std::io::Result<()> {
             server.bind(format!("{}:{}", host, port))?
         }
     };
+
+    info!("Starting server");
     server.run().await
 }
--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,5 +1,5 @@
 mod model;
 mod routes;
 
-pub use model::User;
+pub use model::*;
 pub use routes::init_routes;
